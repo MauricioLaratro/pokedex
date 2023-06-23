b6153e065c52c31604015545cd37e9952781ea82
unknown <Mauricio Laratro>
2023-06-23 05:03:48 -0300

    volvimos a la api de pokemon

--- js/api.js
+++ js/api.js
@@ -1,41 +1,17 @@
-// const BASE_API = 'https://pokeapi.co/api/v2/'
+const BASE_API = 'https://pokeapi.co/api/v2/'
 
-// export async function getPokemon(id){
-//     const response = await fetch(`${BASE_API}pokemon/${id}/`)
-//     const data = await response.json()
-//     // Aca falta validar errores
-//     return data
-// }
-
-// export async function getSpecies(id){
-//     const response = await fetch(`${BASE_API}pokemon-species/${id}/`)
-//     const data = await response.json()
-//     // Aca falta validar errores
-//     return data
-// }
-
-const baseURL = 'https://api.football-data.org/v2/';
-
-export async function getJugador(dorsal) {
-  const token = '5ff15e0245ad4c0f919097d32976e00c'; // Reemplaza "YOUR_API_TOKEN" con tu propio token de API
-
-  const response = await fetch(`${baseURL}players`, {
-    headers: {
-      'X-Auth-Token': token
-    }
-  });
-
-  if (!response.ok) {
-    throw new Error(`Error al obtener los datos del jugador: ${response.status} ${response.statusText}`);
-  }
-
-  const data = await response.json();
-
-  const jugador = data.players.find((player) => player.shirtNumber === dorsal);
-
-  if (!jugador) {
-    throw new Error(`No se encontró ningún jugador con el dorsal ${dorsal}`);
-  }
-
-  return jugador;
+export async function getPokemon(id){
+    const response = await fetch(`${BASE_API}pokemon/${id}/`)
+    const data = await response.json()
+    //Aca falta validar errores
+    return data
 }
+
+export async function getSpecies(id){
+    const response = await fetch(`${BASE_API}pokemon-species/${id}/`)
+    const data = await response.json()
+    //Aca falta validar errores
+    return data
+}
+
+// GFgeuYWtYEwIHu1PipfmRQtYntmwaGmGo3ahC3hv2um8Dv8vyjWApIDTFWPi
--- js/index.js
+++ js/index.js
@@ -1,30 +1,18 @@
-// import { getPokemon, getSpecies } from "./api.js"
+ import { getPokemon, getSpecies } from "./api.js"
 
-// const $form = document.querySelector('#form')
+ const $form = document.querySelector('#form')
+ const $image = document.querySelector('#image')
+ const $description = document.querySelector('#description')
 
-// $form.addEventListener('submit', async (event) => {
-//     event.preventDefault()
-//     const form = new FormData($form)
-//     const id = form.get('id')
-//     const pokemon = await getPokemon(id)
-//     const species = await getSpecies(id)
-//     console.log(pokemon, species)
-// })
+ $form.addEventListener('submit', async (event) => {
+     event.preventDefault()
+     const form = new FormData($form)
+     const id = form.get('id')
+     const pokemon = await getPokemon(id)
+     const species = await getSpecies(id)
+     const description = species.flavor_text_entries.find((flavor) => flavor.language.name === 'es')
 
-
-import { getJugador } from "./api.js"
-
-const $form = document.querySelector('#form')
-
-$form.addEventListener('submit', async (event) => {
-    event.preventDefault()
-    const form = new FormData($form)
-    const dorsal = form.get('id') // Utiliza el valor del formulario como dorsal
-    try {
-        const jugador = await getJugador(dorsal) // Llamada a la función getJugador con el dorsal del jugador
-        console.log(jugador) // Mostrar los datos del jugador en la consola
-        // Utiliza los datos del jugador como desees, como por ejemplo, mostrarlos en la interfaz de usuario
-    } catch (error) {
-        console.error(error) // Manejo de errores en caso de que no se encuentre el jugador o haya un error en la solicitud
-    }
-})
+     $image.src = pokemon.sprites.front_default
+     $description.textContent = description.flavor_text
+     console.log(description)
+ })
